9a9e5b2717d367c507c325768146f2c5be4e6af7
Sam <a@a.a>
2025-10-03 23:58:34 +0700

    Finished problem 2

--- problem_2/problem_2.c
+++ problem_2/problem_2.c
@@ -1,5 +1,17 @@
+#include "fibonacci.h"
 #include <stdio.h>
 
 int main() {
-  // TODO: Make function that calculates fibonacci numbers
+  int sum = 0;
+  int under = 1;
+  int i;
+
+  for (i = 1; fibonacci(i) <= 4000000; i++) {
+    if (fibonacci(i) % 2 == 0) {
+      sum += fibonacci(i);
+    }
+  }
+  printf("Sum of even fibonacci's: %d\n", sum);
+
+  return 0;
 }
