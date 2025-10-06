8df61d021a9270561f180bae9db1f3754c615222
Sam <site-github-slar1@mavramail.com>
2025-10-06 18:00:07 +0700

    Cleaned up problem 3 function find_primes.c

---
+++ problem_7/find_primes.h
@@ -0,0 +1,17 @@
+#ifndef FIND_PRIMES_H
+#define FIND_PRIMES_H
+
+/**
+ * @file find_primes.h
+ * @brief Function declarations find_primes
+ */
+
+/**
+ * @brief Computes prime numbers below given value
+ * @param n The given value
+ * @return Pointer to list of prime numbers
+ */
+
+unsigned long *find_primes(unsigned long n);
+
+#endif
