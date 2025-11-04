5b0ebdb754eea96163047be9770534b48d6842bf
Sam <site-github-slar1@mavramail.com>
2025-11-04 12:56:04 +0700

    Cleaned up 0024, also fixed weird bug where I was going out of bounds looping for k, but it worked for some reason when I included the additional print statements.

--- 0024/main.c
+++ 0024/main.c
@@ -1,15 +1,4 @@
-#include <math.h>
 #include <stdio.h>
-
-long arr2int(int arr[], int arr_len) {
-  long num = 0;
-  long place = 1;
-  for (int i = arr_len - 1; i >= 0; i--) {
-    num += place * arr[i];
-    place *= 10;
-  }
-  return num;
-}
 
 int main() {
   // What is the millionth lexographic permutation of the digits 0, 1, 2, 3, 4,
@@ -23,24 +12,11 @@ int main() {
   int perms = 2;
   int rank = 1000000 + 1;
 
-  // int arr[] = {0, 1, 2, 3};
-  // int arr_len = 4;
-  // int perms = 2;
-  // int rank = 25;
-
-  long prev_num = arr2int(arr, arr_len);
-  long current_num = 0;
-
-  for (int i = 0; i < arr_len; i++) {
-    printf("%d", arr[i]);
-  }
-  printf("\n");
-
   while (perms < rank) {
     // step 1 - find the largest index k such that arr[k] < arr[k + 1]
     int largest_index_k = -1;
 
-    for (int i = arr_len - 1; i >= 0; i--) {
+    for (int i = arr_len - 2; i >= 0; i--) {
       if (arr[i] < arr[i + 1] && i > largest_index_k) {
         largest_index_k = i;
       }
@@ -71,29 +47,19 @@ int main() {
     int count_down = arr_len - 1;
 
     for (int i = largest_index_k + 1; count_down - i >= 0; i++) {
-      // printf("arr_len - i: %d\n", arr_len - i - 1);
-      // printf("switched index %d with index %d\n", i, count_down);
-      temp = arr[i]; // start here
+      temp = arr[i];
       arr[i] = arr[count_down];
       arr[count_down] = temp;
       count_down -= 1;
     }
 
-    current_num = arr2int(arr, arr_len);
-    printf("Previous number: %ld\nCurrent number: %ld\n", prev_num,
-           current_num);
-    if (prev_num > current_num) {
-      printf("Previous is smaller than current: False\n");
-      return 1;
-    } else {
-      printf("Previous is smaller than current: True\n");
+    if (perms == rank - 1) {
+      printf("permutation: %d\n", perms);
+      for (int i = 0; i < arr_len; i++) {
+        printf("%d", arr[i]);
+      }
+      printf("\n");
     }
-    prev_num = current_num;
-    printf("permutation: %d\n", perms);
-    for (int i = 0; i < arr_len; i++) {
-      printf("%d", arr[i]);
-    }
-    printf("\n");
 
     perms++;
   }
